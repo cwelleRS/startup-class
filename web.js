dc4d33624d7332cf4af90bd751fac40a60f3606c
Nikola Cvetkovic <nikola.cvetkovic123@gmail.com>
2013-08-09 18:12:43 +0000

    try with this

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
+
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
     
-response.send('Hello World! 2');
-//response.send(fs.readFileSync(index.html).toString();
+//response.send('Hello World! 2');
+response.send(fs.readFileSync(index.html).toString();
 });
 
 var port = process.env.PORT || 5000;
