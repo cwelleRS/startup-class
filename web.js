e2d62574aed22f3f6702fe682f81994ab200e56b
Nikola Cvetkovic <nikola.cvetkovic123@gmail.com>
2013-08-09 18:40:05 +0000

    Script commit

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 
+
 var express = require('express');
 var fs = require('fs');
 var app = express();
@@ -7,10 +8,15 @@ app.use(express.logger());
 app.get('/', function(request, response) {
     
 //response.send('Hello World! 2');
-response.send(fs.readFileSync(index.html).toString();
+response.send(fs.readFileSync("./index.html").toString();
+console.log ("Read file")
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var buffer = new Buffer(256);
+buffer = fs.readFileSync(index.html);
+var content = buffer.toString();
